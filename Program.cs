3a43fc7f7ef6dedc16a4257bd2a86997cbb3986b
Jennifer Massi <jennifer.massi@mscoding.org>
2023-01-08 08:41:54 -0800

    Parsing User Input

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Halloween
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string Userinput = "";
+            int number = 0;
+
+            Console.Write("How old are you? ");
+            Userinput = Console.ReadLine();
+          
+            number = int.Parse(Userinput);
+            Console.WriteLine("You will be " + (number + 15 ) + " in 15 years");
+
+
+        }
+    }
+}
